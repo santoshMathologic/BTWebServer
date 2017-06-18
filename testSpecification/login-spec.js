71036cef1efc5c6f507e0467d0008b94623de0c1
rakesh <rak1994kumar@gmail.com>
2017-06-18 22:54:36 +0530

    request npm added

---
+++ testSpecification/login-spec.js
@@ -0,0 +1,14 @@
+
+
+var logIn = require("../routes/login");
+var request = require('request');
+
+describe("login function", function () {
+
+    it("should be username and password correct", function () {
+        //var result = logIn.userLogin()
+        //expect(result).toBe(6);
+    });
+
+   
+});
