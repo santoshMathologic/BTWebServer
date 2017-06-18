991a2ca4a0a25a6cb59f712160d91e917dd67c78
rakesh <rak1994kumar@gmail.com>
2017-06-18 23:49:58 +0530

    testing working perfect

--- testSpecification/login-spec.js
+++ testSpecification/login-spec.js
@@ -3,12 +3,32 @@
 var logIn = require("../routes/login");
 var request = require('request');
 
+var username = "test";
+var password = "test";
+var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+var url = "http://localhost:4000/login/";
+
+
 describe("login function", function () {
 
     it("should be username and password correct", function () {
         //var result = logIn.userLogin()
         //expect(result).toBe(6);
+
+        request.post({
+            url: url,
+            headers: {
+                "Authorization": auth
+            }
+        }, function (error, response, body) {
+            console.log('body : ', body);
+            console.log('body : ', error);
+            console.log('body : ', response);
+        });
+
+
+
     });
 
-   
+
 });
