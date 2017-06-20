38175df3865b5d8b84962cf5066c84205deea27a
rakesh <rak1994kumar@gmail.com>
2017-06-20 19:08:00 +0530

    login success with token

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -7,13 +7,7 @@ var auth = require('basic-auth');
 var jwt = require("jwt-simple");
 var moment = require('moment');
 moment().format();
-
 var config = require("../../config/config");
-
-
-
-// encode 
-
 
 exports.userLogin = function (req, res) {
 
@@ -22,19 +16,24 @@ exports.userLogin = function (req, res) {
         validate(credentials.name, credentials.pass).then(function (result) {
 
             if (credentials.name === result.username && credentials.pass === result.password) {
-                var token  = generateToken(result.username, result.role);
-                var decoded = jwt.decode(token, config.secret, false, 'HS512');
-                console.log(decoded);  
-              //  console.log(token);
+                var tokenObj = generateToken(result.username, result.role);
+                //var decoded = jwt.decode(token, config.secret, false, 'HS512');
+                //console.log(decoded);
+                //  console.log(token);
+                res.cookie('x-access-token',tokenObj.token , { maxAge: tokenObj.expire,httpOnly:true} );
+                res.cookie('x-key', tokenObj.user);
                 res.status(200);
                 resultVal = {
-                    "status": 200,
-                    "statusText": "Validat credentials",
+                    status: 200,
+                    message: "Authentication Success !!!!!!",
+                    success: true,
+                    tokenKey:tokenObj
                 };
-
                 return res.json(resultVal);
             } else
-                return res.json("login Unsuccessfully");
+            
+            res.status(401); 
+                res.json({ status: 401, success: false, message: 'Authentication failed. Unauthorized User!!!' });
 
         });
 
@@ -53,8 +52,15 @@ generateToken = function (user, roleCode) {
     var d = new Date();
     var expires = d.expireOn("7", "other");
     var payload = { username: user, role: roleCode };
-    var token = jwt.encode({ payload: payload, exp: expires }, config.secret,'HS512');
-    return token;
+    var token = jwt.encode({ payload: payload, exp: expires }, config.secret, 'HS512');
+
+     return {
+         "user": user,
+         "roleCode":roleCode,
+         "expire" :expires,
+         "token" : token
+     };
+    
 };
 
 Date.prototype.expireOn = function (noOfDays, ch) {
@@ -80,17 +86,17 @@ validate = function (username, pass) {
     var query = userModel.findOne({ username: username, password: pass }).populate('roleId').exec(function (err, result) {
         if (err) throw new Error("User Not found");
         else {
-            if (result.length !== null) {
 
+            console.log("DASDAS");
+            if (result !== null) {
                 dbUserObj = { // spoofing a userobject from the DB. 
                     username: result._doc.username,
                     password: result._doc.password,
                     role: result._doc.roleId._doc.name
                 };
                 deferred.resolve(dbUserObj);
-            } else {
-                deferred.resolve(result);
             }
+             deferred.resolve("User Not Found ");
         }
     });
 
