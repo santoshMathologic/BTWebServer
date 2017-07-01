e0550e7528a2d920fdbf21b3c2e43f2f9248da11
rakesh <rak1994kumar@gmail.com>
2017-07-01 18:28:06 +0530

    changes in package.json

--- database/db.js
+++ database/db.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1/bt?maxPoolSize=200', function(error) {
+
+var options = {
+  db: { native_parser: true },
+  server: { poolSize: 5 },
+  replset: { rs_name: 'myReplicaSetName' },
+  user: '',
+  pass: ''
+};
+
+mongoose.connect('mongodb://127.0.0.1/bt?maxPoolSize=200',options, function(error) {
     if (error) {
         console.log('Cant connect to db', error);
     } else {
